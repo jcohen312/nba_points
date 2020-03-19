133fcccb9b5f9fa7ba44c4771ca27ba0b68a42aa
Jake Cohen <jakecohen212@gmail.com>
2020-03-19 13:24:26 -0400

    dynamic season and player toggle

--- app.py
+++ app.py
@@ -1,37 +1,53 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Output, Input
+from dash.dependencies import Output, Input, State
 from shot_viz import shot_chart
 
 app = dash.Dash()
 
 app.layout = html.Div([
-    html.Div(html.H1(children = "NBA Clustering Project")),
-    html.Label("Shot Chart"),
+        html.Div(
+            html.H1(children = "NBA Clustering Project")
+        ),
 
-    html.Div(
-        dcc.Input(
-            id='player_input',
-            placeholder='PLAYER NAME',
-            type='text',
-            value=''
-        )
-    ),
+        html.Label("Shot Chart"),
 
-    html.Div(
-        dcc.Graph(id='Shot Chart',
-                  )
-    ),
+        html.Div(
+            [dcc.Input(
+                id='player_input',
+                placeholder='PLAYER NAME',
+                type='text',
+                value=''
+            ),
+            dcc.Dropdown(
+              id="season-dropdown",
+              options=[
+                  {'label': '2017', 'value': '2017'},
+                  {'label': '2018', 'value': '2018'},
+                  {'label': '2019', 'value': '2019'}
+              ],
+              value='2018'
+            ),
+
+            ]
+        ),
+
+        html.Div(
+            dcc.Graph(id='Shot Chart',
+                      )
+        ),
 
 ])
 
 @app.callback(dash.dependencies.Output("Shot Chart", "figure"),
-              [dash.dependencies.Input("player_input", "value")]
+              [Input("player_input", "value"),
+               Input("season-dropdown", "value")],
+
               )
 
-def update_shot_chart(input_value):
-    return shot_chart(input_value, '2018')
+def update_shot_chart(player, year):
+    return shot_chart(player, year)
 
 if __name__== "__main__":
     app.run_server(debug=True)
